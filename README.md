520aad9c64d8568ee2975fcaa6cc398f0c43ac6e
Michael Tracy <michael@luckywabbit.com>
2016-02-08 22:01:06 -0500

    Update readme, add demo video link

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# Hello World
+# P1 - Portfolio
 
-Just for practice
+## Live URL
+<http://p1.luckywabbit.com>
+
+## Description
+Set up proof of concept and intro
+
+## Demo
+https://drive.google.com/file/d/0B3nqEWHg6Uy3RFRkc2FKUVQxbEE/view?pref=2&pli=1
+
+## Details for teaching team
+No login required.
+
+
+## Outside code
+* jQuery: https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js 
+* Bootstrap: https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css 
+* Google Fonts: (Poiret One): https://fonts.googleapis.com/css?family=Poiret+One
+ 
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>DWA - Hello World</title>
+<title>DWA - P1</title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
 <link href='https://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
@@ -19,20 +19,20 @@
   </div>
   <div class="row">
     <div class="col-md-12">
-      <h2>P1 - Portfolio</h2>
-      <img src="img/tmt.jpg"/>
-        <h3>Project 2 Title</h3>
-		<p>View: <a href="#">[link]</a></p>
-		<p>Github: <a href="#">[link]</a></p>
-
-		<h3>Project 3 Title</h3>
-		<p>View: <a href="#">[link]</a></p>
-		<p>Github: <a href="#">[link]</a></p>
-
-		<h3>Project 4 Title</h3>
-		<p>View: <a href="#">[link]</a></p>
-		<p>Github: <a href="#">[link]</a></p>
-
+    <h2>P1 - Portfolio</h2>
+      <div class="demoVid"><a href="https://drive.google.com/file/d/0B3nqEWHg6Uy3RFRkc2FKUVQxbEE/view?usp=sharing"  target="_blank">
+        <img src="img/tmt.jpg"/>
+        <p> P1 Demo Video - Click Here</p>
+        </a> </div>
+      <h3>Project 2 Title</h3>
+      <p>View: <a href="#">[link]</a></p>
+      <p>Github: <a href="#">[link]</a></p>
+      <h3>Project 3 Title</h3>
+      <p>View: <a href="#">[link]</a></p>
+      <p>Github: <a href="#">[link]</a></p>
+      <h3>Project 4 Title</h3>
+      <p>View: <a href="#">[link]</a></p>
+      <p>Github: <a href="#">[link]</a></p>
     </div>
   </div>
 </div>
